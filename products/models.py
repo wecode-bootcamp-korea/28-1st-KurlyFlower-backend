14ce485276884aff462508f73aea43d1870004a1
MinWook Jang <jmw1080@gmail.com>
2021-12-28 18:10:02 +0900

    코딩 컨벤션 준수

--- products/models.py
+++ products/models.py
@@ -3,16 +3,16 @@ from django.db.models.fields import DecimalField
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=300)
-    description = models.CharField(max_length=500)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    units = models.CharField(max_length=20)
-    weights = models.DecimalField(max_digits=10, decimal_places=1)
-    shipping_type = models.CharField(max_length=30, default="샛별배송")
-    origin = models.CharField(max_length=200)
-    thumbnail_url = models.URLField()
-    category = models.ForeignKey("Category", on_delete=models.CASCADE)
-    subcategory = models.ForeignKey("Subcategory", on_delete=models.CASCADE)
+    name           = models.CharField(max_length=300)
+    description    = models.CharField(max_length=500)
+    price          = models.DecimalField(max_digits=10, decimal_places=2)
+    units          = models.CharField(max_length=20)
+    weights        = models.DecimalField(max_digits=10, decimal_places=1)
+    shipping_type  = models.CharField(max_length=30, default="샛별배송")
+    origin         = models.CharField(max_length=200)
+    thumbnail_url  = models.URLField()
+    category       = models.ForeignKey("Category", on_delete=models.CASCADE)
+    subcategory    = models.ForeignKey("Subcategory", on_delete=models.CASCADE)
     packaging_type = models.ManyToManyField("Packaging_Type")
 
     class Meta:
@@ -25,14 +25,14 @@ class Category(models.Model):
         db_table = "categories"
 
 class Subcategory(models.Model):
-    name = models.CharField(max_length=50)
+    name     = models.CharField(max_length=50)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
 
     class Meta:
         db_table = "subcategories"
 
 class Image(models.Model):
-    url = models.URLField()
+    url     = models.URLField()
     product = models.ForeignKey("Product", on_delete=models.CASCADE)
 
     class Meta:
