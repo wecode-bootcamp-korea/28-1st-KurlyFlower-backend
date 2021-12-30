7f5bca42ad8d39b6d1394156937284c50fb633a9
Chanjoo lee <dlckswn14@gmail.com>
2021-12-30 23:20:11 +0900

    카테고리 전체 조회 api  추가

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
+from django.views import View
 
-# Create your views here.
+from products.models import Category
+
+class CategoryView(View):
+    def get(self, request):
+        categories = Category.objects.all()
+
+        result = []
+        for category in categories:
+            subcategories = category.subcategory_set.filter(category_id=category.id).values()
+            result.append(
+                {
+                    "id": category.id,
+                    "name": category.name,
+                    "subcategories": [sub for sub in subcategories]
+                }
+            )
+        return JsonResponse({"result":result}, status=200)
