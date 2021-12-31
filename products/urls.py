97a8e29b39e0ea5c9511228728d22881526b48d5
Chanjoo lee <dlckswn14@gmail.com>
2021-12-31 16:56:42 +0900

    카테고리별 상품 조회 가능한 api 추가

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from products.views import CategoryView
+from products.views import CategoryView, CollectionView, ProductListView
 
 urlpatterns = [
     path("categories", CategoryView.as_view()),
+    path("products", ProductListView.as_view()),
+    path("products/collection/<int:collection_id>", CollectionView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.http.response import JsonResponse
 from django.views import View
 
-from products.models import Category
+from products.models import Category, Product
 
 class CategoryView(View):
     def get(self, request):
@@ -12,9 +12,40 @@ class CategoryView(View):
             subcategories = category.subcategory_set.filter(category_id=category.id).values()
             result.append(
                 {
-                    "id": category.id,
-                    "name": category.name,
+                    "id"           : category.id,
+                    "name"         : category.name,
                     "subcategories": [sub for sub in subcategories]
                 }
             )
         return JsonResponse({"result":result}, status=200)
+
+class ProductListView(View):
+    def get(self, request):
+        query = Product.objects.order_by("id")
+
+        category_id    = request.GET.get("category_id")
+        subcategory_id = request.GET.get("subcategory_id")
+
+        if category_id:
+            category_id = int(category_id)
+            query = query.filter(category_id=category_id)
+        if category_id and subcategory_id:
+            category_id = int(category_id)
+            subcategory_id = int(subcategory_id)
+            query = query.filter(category_id=category_id, subcategory_id=subcategory_id)
+
+        products = query[:20]
+
+        result = [
+            {
+                "id"            : product.id,
+                "category_id"   : product.category_id,
+                "subcategory_id": product.subcategory_id,
+                "name"          : product.name,
+                "description"   : product.description,
+                "price"         : product.price,
+                "thumbnail_url" : product.thumbnail_url
+            }
+            for product in products
+        ]
+        return JsonResponse({"result": result}, status=200)
