86cf333365f7c0f7b5696fbc88ab8216d995a526
MinWook Jang <jmw1080@gmail.com>
2022-01-02 12:07:19 +0900

    컨플릭트 수정2

--- kurlyflower/urls.py
+++ kurlyflower/urls.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 from django.urls import path, include
 
 urlpatterns = [
-    path('users', include('users.urls'))
-=======
-from django.urls import path
-from django.urls.conf import include
-
-urlpatterns = [
-    path("", include("users.urls")),
->>>>>>> main
+    path('users', include('users.urls')),
 ]
