6a0bd4bc3ccccc13f5ec2c9995be7b2cd5b0129c
MinWook Jang <jmw1080@gmail.com>
2022-01-04 11:56:00 +0900

    디테일 페이지 구현

---
+++ products/urls.py
@@ -0,0 +1,6 @@
+from django.urls    import path
+from products.views    import ProductDetailView
+
+urlpatterns = [
+     path("/<int:product_id>", ProductDetailView.as_view()),
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http      import JsonResponse
+from django.views     import View
+from products.models  import Product
 
-# Create your views here.
+
+class ProductDetailView(View):
+    def get(self, request, product_id):
+
+        try:
+            product   = Product.objects.get(id=product_id)
+            packaging = product.packaging.get(product=product_id)
+            images    = product.image_set.all()
+                        #product - 중간테이블(product_id, pakaging_id) - pakaging테이블
+            data = {
+                "name"          : product.name,
+                "description"   : product.description,
+                "price"         : int(product.price),
+                "units"         : product.units,
+                "weights"       : product.weights,
+                "shipping_type" : product.shipping_type,
+                "origin"        : product.origin,
+                "packaging"     : packaging.name,
+                "detail_images" : [image.url for image in images],
+                "thumbnail_url" : product.thumbnail_url
+            }
+            return JsonResponse({"product_detail":data}, status = 201)
+
+        except Product.DoesNotExist:
+            return JsonResponse({"message":"Product_DoesNotExist"}, status=404)
