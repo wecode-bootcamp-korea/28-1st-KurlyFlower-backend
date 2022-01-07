df8582615c3651130c65cfa2d745f571f8901c64
Chanjoo lee <dlckswn14@gmail.com>
2022-01-07 21:14:59 +0900

    try except 범위수정

--- users/decorators.py
+++ users/decorators.py
@@ -12,7 +12,6 @@ def login_required(func):
             payload      = jwt.decode(access_token, SECRET_KEY, algorithms=ALGORITHM)
             user         = User.objects.get(id=payload["id"])
             request.user = user
-            return func(self, request, *args, **kwargs)
 
         except KeyError:
             return JsonResponse({"message":"NO_TOKEN"}, status=400)
@@ -25,5 +24,7 @@ def login_required(func):
 
         except User.DoesNotExist:
             return JsonResponse({"MESSAGE": "INVALID_USER"}, status=400)
+
+        return func(self, request, *args, **kwargs)
 
     return wrapper
